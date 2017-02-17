f87f0caad7f5b5d94a728b4d4da261dc26853259
YannGarcia <garcia.yann@gmail.com>
2017-02-17 14:08:43 +0100

    Update TestBtp sample Add DNS sample

---
+++ BtpTitan/src/BtpEncodeDecode.cc
@@ -0,0 +1,14 @@
+#include "Btp_EncodeDecode.hh"
+
+namespace Btp__EncodeDecode {
+
+///****************************************************
+// * @desc    External function to encode a SecuredMessage type
+// * @param   value to encode
+// * @return  encoded value
+// ****************************************************/
+//INTEGER fx__decvalue(const BITSTRING& p__raw, Btp__TypesAndValues::Frame& p__frame) {
+//  return -1;
+//}
+
+} // end namespace
