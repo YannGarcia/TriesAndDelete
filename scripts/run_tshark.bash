5beaceee590a3f2525243d8e9546f945036de177
vagrant <vagrant@vagrant.vm>
2018-02-16 09:37:35 +0100

    Rebuild ITS dissectors

--- scripts/run_tshark.bash
+++ scripts/run_tshark.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Debug mode
-set -evx
+#set -vx
+set -e
 
-tshark -ienp0s8 -V -f"ether proto 0x8947" -Tfields -eframe.time -eeth.dst -eeth.src -eeth.type
+tshark -ieth1 -V -f"ether proto 0x8947" -Tfields -eframe.time -eeth.dst -eeth.src -eeth.type -edata
