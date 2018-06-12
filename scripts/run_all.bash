a51a1171ef033a3af7a6a319e80aa2a25c670283
vagrant <yann.garcia@fscom.fr>
2018-06-12 13:38:01 +0200

    Add LD_LIBRARY_PATH for oSIP2

--- scripts/run_all.bash
+++ scripts/run_all.bash
@@ -4,6 +4,8 @@
 
 clear
 
+export LD_LIBRARY_PATH=/home/vagrant/frameworks/osip/src/osipparser2/.libs:$LD_LIBRARY_PATH
+
 if ! [[ $1 =~ "^[0-9]+$" ]]
 then
     COUNTER=$1
@@ -26,8 +28,8 @@ rm ../logs/merged.log.*
 
 for i in $(seq 1 1 $COUNTER)
 do
-    ../bin/run_mtc.bash &
-    ../bin/run_ptcs.bash
+    LD_LIBRARY_PATH=/home/vagrant/frameworks/osip/src/osipparser2/.libs:$LD_LIBRARY_PATH ../bin/run_mtc.bash &
+    LD_LIBRARY_PATH=/home/vagrant/frameworks/osip/src/osipparser2/.libs:$LD_LIBRARY_PATH ../bin/run_ptcs.bash
 
     dup=$(ps -ef | grep "$0" | grep -v grep | wc -l)
     while [ ${dup} -eq 3 ]
