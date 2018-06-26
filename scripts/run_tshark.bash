3c57f39c3309a1bcf274f514efdce8a93b10e9ea
vagrant <yann.garcia@fscom.fr>
2018-06-26 16:26:13 +0200

    Add memory check flags on compiler/linker command line

--- scripts/run_tshark.bash
+++ scripts/run_tshark.bash
@@ -4,4 +4,4 @@
 #set -vx
 set -e
 
-~/frameworks/wireshark-build/run/tshark -ieth1 -V -f"ether proto 0x8947" -Tfields -eframe.time -eeth.dst -eeth.src -eeth.type -edata
+~/frameworks/wireshark-build/run/tshark -ieth1 -V -f"ether proto 0x8947 or udp src port 12345 or udp dst port 12345" -Tfields -eframe.time -eeth.dst -eeth.src -eeth.type -edata
